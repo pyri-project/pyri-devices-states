9ceac739fe33f48e08cbed2515274eb2304bbf76
John Wason <wason@wasontech.com>
2022-12-31 03:31:36 -0500

    Use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,32 +1,11 @@
 from setuptools import setup, find_packages, find_namespace_packages
 
 setup(
-    name='pyri-devices-states',
-    version='0.1.0',
-    description='PyRI Teach Pendant Devices States Service',
-    author='John Wason',
-    author_email='wason@wasontech.com',
-    url='http://pyri.tech',
     package_dir={'': 'src'},
     packages=find_namespace_packages(where='src'),
     include_package_data=True,
     package_data = {
         'pyri.devices_states': ['*.robdef','*.yml']
     },
-    zip_safe=False,
-    install_requires=[
-        'pyri-common',
-        'pyri-device-manager',
-        'robotraconteur'
-    ],
-    tests_require=['pytest','pytest-asyncio'],
-    extras_require={
-        'test': ['pytest','pytest-asyncio']
-    },
-    entry_points = {
-        'pyri.plugins.robdef': ['pyri-sandbox-robdef=pyri.devices_states.robdef:get_robdef_factory'],
-        'pyri.plugins.device_type_adapter': ['pyri-device-states-type-adapter = pyri.devices_states.device_type_adapter:get_device_type_adapter_factory'],
-        'console_scripts': ['pyri-devices-states-service = pyri.devices_states.__main__:main'],
-        'pyri.plugins.service_node_launch': ['pyri-devices-states-launch = pyri.devices_states.service_node_launch:get_service_node_launch_factory']
-    }
+    zip_safe=False       
 )
